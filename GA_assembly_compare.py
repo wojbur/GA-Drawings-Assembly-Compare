1fbe465f22a5ebe1a21b57950cbf835330b42c1d
Wojciech Burman <wojbur94@gmail.com>
2022-10-22 18:53:22 +0200

    add finding and comparing mark functionality

--- GA_assembly_compare.py
+++ GA_assembly_compare.py
@@ -1,35 +1,39 @@
-"""A tool used to search content of steel construction General Arrangement PDF drawings to find if all steel asseblies are listed"""
+"""A tool used to search content of PFD files of steel construction General Arrangement drawings to find if all steel asseblies
+have corresponding assembly marks. The tool reads all text from GA drawings, searches for assembly marks and compares the results
+with list of assembly drawings"""
 
+
+from turtle import width
 from PyPDF2 import PdfReader
 from pathlib import Path
 import re
 import tkinter as tk
 import tkinter.filedialog as fd
+import tkinter.scrolledtext as st
 
 
-# Define display window
+# Set display window
 window = tk.Tk()
 window.title('GA - Assembly compare')
 
-
 # Define application class
-class GAAssemblyCompare:
+class GaAssemblyCompare:
     def __init__(self, main):
+        self.main = main
 
         # Define list of files to compare
         self.ga_dwgs = []
         self.assy_dwgs = []
 
-        # Define assembly numbering pattern
-        self.pattern = re.compile(r'^\d+[A-Z]{1,3}\d+$')
-
         # Define main frames grid
-        self.frame1 = tk.Frame(main, width=100, borderwidth=5)
-        self.frame2 = tk.Frame(main, width=100, borderwidth=5)
-        self.frame3 = tk.Frame(main, width=100, borderwidth=5)
-        self.frame1.grid(row=0, column=0)
-        self.frame2.grid(row=0, column=1)
-        self.frame3.grid(row=0, column=2)
+        self.frame1 = tk.Frame(self.main, width=100, borderwidth=5)
+        self.frame2 = tk.Frame(self.main, width=100, borderwidth=5)
+        self.frame3 = tk.Frame(self.main, width=100, borderwidth=5)
+        self.frame4 = tk.Frame(self.main, width=100, borderwidth=5)
+        self.frame1.grid(row=0, column=0, padx=2)
+        self.frame2.grid(row=0, column=1, padx=2)
+        self.frame3.grid(row=0, column=2, padx=2)
+        self.frame4.grid(row=0, column=3, padx=2)
 
         # Define assembly and ga drawings listboxes
         self.assy_lstbox = tk.Listbox(self.frame1, width=30, height=30, selectmode=tk.EXTENDED)
@@ -44,31 +48,55 @@ class GAAssemblyCompare:
         self.ga_scrollbar.grid(row=1, column=0, sticky=tk.NS)
         self.ga_lstbox.config(yscrollcommand=self.ga_scrollbar)
 
-        # Define test buttons
-        self.button_test1 = tk.Button(self.frame3, text='print_assy_dwgs', command=self.print_assy_dwgs)
-        self.button_test1.grid(row=0, column=0)
-    
-        self.button_test2 = tk.Button(self.frame3, text='print_ga_dwgs', command=self.print_ga_dwgs)
-        self.button_test2.grid(row=1, column=0)
-
-        self.button_test2 = tk.Button(self.frame3, text='compare dwgs', command=self.compare_drawings)
-        self.button_test2.grid(row=2, column=0)
-
         # Define buttons
         self.button_add_assy = tk.Button(self.frame1, text='Select assembly drawings', command=self.select_assy_dwgs)
         self.button_add_assy.grid(row=0, column=0, columnspan=2, sticky=tk.EW)
 
         self.button_add_ga = tk.Button(self.frame2, text='Select GA drawings', command=self.select_ga_dwgs)
         self.button_add_ga.grid(row=0, column=0, columnspan=2, sticky=tk.EW)
+
+        self.button_compare = tk.Button(self.frame3, text='Compare marks \u279d', command=self.compare_drawings, width=15)
+        self.button_compare.grid(row=0, column=0, columnspan=2)
+
+        # Define output text widget
+        self.output_txt = st.ScrolledText(self.frame4, width=30, height = 30)
+        self.output_txt.grid(row=1, column=0)
+        self.output_txt.insert(tk.INSERT, "Assembly marks printed in white/invisible color will be treated as visible!")
+        self.output_txt.config(background='gray92', state='disabled', wrap='word')
+
+        # Define drop down menu to select assembly mark pattern
+        self.pattern = None
+
+        self.dropdown_label = tk.Label(self.frame3, text='FAB:', width=5)
+        self.dropdown_label.grid(row=1, column=0)
+
+        self.fab_options = ['','ALM', 'BSC', 'TSC', 'CSS']
+        self.fab = tk.StringVar()
+        self.fab.set(self.fab_options[0])
+
+        self.dropdown = tk.OptionMenu(self.frame3, self.fab, *self.fab_options, command=self.pattern_changed)
+        self.dropdown.grid(row=1, column=1, sticky=tk.EW)
+
+        self.pattern_label = tk.Label(self.frame3)
+        self.pattern_label.grid(row=2, column=0, columnspan=2)
     
     # Test functions
-    def print_assy_dwgs(self):
-        for dwg in self.assy_dwgs:
-            print(dwg)
-
-    def print_ga_dwgs(self):
-        for dwg in self.ga_dwgs:
-            print(dwg)
+    def pattern_changed(self, *args):
+        if self.fab.get() == '':
+            self.pattern_label['text'] = ''
+            self.pattern = None
+        elif self.fab.get() == 'ALM':
+            self.pattern_label['text'] = 'num pattern: 1B234'
+            self.pattern = re.compile(r'^\d+[A-Z]{1,3}\d+$')
+        elif self.fab.get() == 'TSC':
+            self.pattern_label['text'] = 'num pattern: 1234B'
+            self.pattern = re.compile(r'^\d{4,5}[A-Z]{1,2}$')
+        elif self.fab.get() == 'BSC':
+            self.pattern_label['text'] = 'num pattern: 7B134'
+            self.pattern = re.compile(r'^\d+[A-Z]{1,3}\d+$')
+        elif self.fab.get() == 'CSS':
+            self.pattern_label['text'] = 'num pattern: 7B134'
+            self.pattern = re.compile(r'^\d+[A-Z]{1,3}\d+$')
     
 
     # Define button functions
@@ -108,23 +136,49 @@ class GAAssemblyCompare:
             lstbox.insert(tk.END, file[1])
     
     def compare_drawings(self):
+        self.output_txt.config(state='normal')
+        self.output_txt.delete('1.0', tk.END)
+
+        if not self.assy_dwgs:
+            self.output_txt.insert(tk.INSERT, 'Select assembly drawings!')
+            self.output_txt.config(state='disabled')
+            return
+        if not self.ga_dwgs:
+            self.output_txt.insert(tk.INSERT, 'Select GA drawings!')
+            self.output_txt.config(state='disabled')
+            return
+        if not self.pattern:
+            self.output_txt.insert(tk.INSERT, 'Select Fabricator!')
+            self.output_txt.config(state='disabled')
+            return
+
         assy_on_ga = set()
         for dwg in self.ga_dwgs:
             reader = PdfReader(dwg[0])
             page = reader.pages[0]
-            print(f'Searching {dwg[1]}')
+            self.output_txt.insert(tk.INSERT, f'Searching {dwg[1]}...\n')
             page_text = page.extract_text()
             page_lst = page_text.splitlines()
             page_assy_lst = {i for i in page_lst if self.pattern.match(i)}
             assy_on_ga.update(page_assy_lst)
-        print(assy_on_ga)
+            self.main.update()
+        print('finished')
 
         assy = {i[1] for i in self.assy_dwgs}
-        print(assy)
 
-        print(assy.issubset(assy_on_ga))
+        if assy.issubset(assy_on_ga):
+            self.output_txt.delete('1.0', tk.END)
+            self.output_txt.insert(tk.INSERT, "OK. All assemblies are shown on GA drawings.")
+        
+        else:
+            assy_not_shown = sorted(list(assy - assy_on_ga))
+            self.output_txt.delete('1.0', tk.END)
+            self.output_txt.insert(tk.INSERT, "Missing assembly marks on GA drawings:")
+            for i in assy_not_shown:
+                self.output_txt.insert(tk.INSERT, f'\n{i}')
 
+        self.output_txt.config(state='disabled')
 
 # Create app object
-app = GAAssemblyCompare(window)
+app = GaAssemblyCompare(window)
 window.mainloop()
